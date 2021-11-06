984b0aad761bb70c6fa7f57488c79bd840735147
Riter <767641495@qq.com>
2021-11-07 07:02:02 +0800

    修复sysuser中的构造方法

--- src/main/java/com/example/demo/entity/SysUser.java
+++ src/main/java/com/example/demo/entity/SysUser.java
@@ -1,10 +1,12 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 public class SysUser implements Serializable{
 
     /** 用户ID */
